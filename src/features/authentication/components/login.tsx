32c72a38e34fa7fd443be4067edab98c5550e189
Ilham Rusydi Abd <abd.ilhamrusydi@gmail.com>
2024-10-04 23:23:14 +0700

    feat (login): add usestate and component

--- src/features/authentication/components/login.tsx
+++ src/features/authentication/components/login.tsx
@@ -2,8 +2,27 @@ import { Input } from "../../../components/ui/input";
 import { Button } from "../../../components/ui/button";
 import { AuthLayout } from "./auth.layout";
 import { AtSign, Lock } from "lucide-react";
+import { useState } from "react";
+import { useMutation } from "@tanstack/react-query";
+import { loginUser } from "../services/login";
+import { useNavigate } from "react-router-dom";
 
 export const Login = () => {
+  const navigate = useNavigate();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const {
+    mutate: handleSubmitLogin,
+    isPending,
+    isError,
+    error,
+  } = useMutation({
+    mutationKey: ["login"],
+    mutationFn: () => loginUser({ email, password }),
+    onSuccess: () => navigate("/"),
+  });
+
   return (
     <AuthLayout>
       <div className="w-[300px] space-y-4">
@@ -12,10 +31,16 @@ export const Login = () => {
           <p>welcome back!</p>
         </section>
         <section className="space-y-3">
-          <Input withIcon icon={<AtSign size={16} />} placeholder="Email" />
-          <Input withIcon icon={<Lock size={16} />} placeholder="Password" type="password" />
-          <Button className="flex w-full justify-center">Login</Button>
+          <Input withIcon icon={<AtSign size={16} />} placeholder="Email" onChange={(e) => setEmail(e.target.value)} />
+          <Input withIcon icon={<Lock size={16} />} placeholder="Password" type="password" onChange={(e) => setPassword(e.target.value)} />
+          <Button disabled={isPending} onClick={() => handleSubmitLogin()} className="flex w-full justify-center">
+            Login
+          </Button>
+          <Button isFull variant="outline">
+            Continue with Google
+          </Button>
         </section>
+        {isError && <div className="text-center text-sm font-medium text-red-500">{error.message}</div>}
       </div>
     </AuthLayout>
   );
