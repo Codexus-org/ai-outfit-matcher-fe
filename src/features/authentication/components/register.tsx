c5ddc92d9a47e666c8a04c86b2d062979456c2f3
Ilham Rusydi Abd <abd.ilhamrusydi@gmail.com>
2024-10-04 23:26:25 +0700

    style: changed position

--- src/features/authentication/components/register.tsx
+++ src/features/authentication/components/register.tsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { Button } from "../../../components/ui/button";
 import { Input } from "../../../components/ui/input";
 import { AuthLayout } from "./auth.layout";
-import { AtSign, Lock, SquareUserRound } from "lucide-react";
+import { AtSign, Lock, SquareUserRound, Verified } from "lucide-react";
 import { useMutation } from "@tanstack/react-query";
 import { registerUser } from "../services/register";
 export const Register = () => {
@@ -22,10 +22,10 @@ export const Register = () => {
 
   return (
     <AuthLayout>
-      <div className="w-[300px] space-y-4">
-        <section>
-          <h3>Register</h3>
-          <p>Please create an account to continue</p>
+      <div className="w-[300px] space-y-9">
+        <section className="space-y-2">
+          <h3 className="text-center">Register</h3>
+          <p className="text-center">Please create an account to continue</p>
         </section>
         <section className="space-y-3">
           <section className="flex gap-3">
@@ -36,7 +36,7 @@ export const Register = () => {
             <Input withIcon icon={<SquareUserRound size={16} />} placeholder="Username" onChange={(e) => setUserName(e.target.value)} />
             <Input withIcon icon={<AtSign size={16} />} placeholder="Email" onChange={(e) => setEmail(e.target.value)} />
             <Input withIcon icon={<Lock size={16} />} placeholder="Password" type="password" onChange={(e) => setPassword(e.target.value)} />
-            <Button isFull disabled={isPending} onClick={() => handleSubmitRegister()} className="flex w-full justify-center">
+            <Button isFull disabled={isPending} onClick={() => handleSubmitRegister()}>
               Register
             </Button>
             {isError && <div className="text-center text-sm font-medium text-rose-600">{error?.message}</div>}
@@ -45,9 +45,10 @@ export const Register = () => {
 
         <hr />
         <div>
-          <Button variant="outline" startContent isFull={true}>
+          <Button isFull variant="outline" startContent={<Verified size={16} />} onClick={() => handleSubmitRegister()}>
             Register with Google
           </Button>
+          {isError && <div className="text-center text-sm font-medium text-rose-600">{error?.message}</div>}
         </div>
         <div>
           Already have an account? <a href="/login">Login</a>
