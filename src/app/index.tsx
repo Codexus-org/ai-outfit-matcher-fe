0f274597910dbbd47233558e35f37db2b07037e0
AndiMahardika <andi.mahardika.mulya@gmail.com>
2024-10-05 13:54:31 -0700

    edit: file routes

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,5 +1,5 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { AppRouter } from "./route";
+import { AppRouter } from "./routes";
 
 const queryClient = new QueryClient();
 
--- src/app/routes.tsx
+++ src/app/routes.tsx
@@ -1,14 +1,18 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom"
-import PromptPage from "../features/promptpage/components"
-import Collections from "../features/collections/components"
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Login } from "../features/authentication/components/login";
+import { Register } from "../features/authentication/components/register";
+import PromptPage from "../features/promptpage/components";
+import Collections from "../features/collections/components";
 
-export const AppRoutes = () => {
+export const AppRouter = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<PromptPage />}/>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/" element={<PromptPage />} />
         <Route path="/collections" element={<Collections />} />
       </Routes>
     </BrowserRouter>
-  )
-}
+  );
+};
