20a60c7b35943d199e7bac5f07a1153b1d64a379
Ilham Rusydi Abd <abd.ilhamrusydi@gmail.com>
2024-10-06 01:26:54 +0700

    style: make edge shape curve

--- src/features/authentication/components/auth.layout.tsx
+++ src/features/authentication/components/auth.layout.tsx
@@ -1,10 +1,10 @@
-import React from "react";
+import React from 'react';
 
 export const AuthLayout = ({ children }: React.PropsWithChildren) => {
-  return (
-    <main className="grid h-screen grid-cols-2">
-      <section className="bg-primary-600" />
-      <section className="flex items-center justify-center">{children}</section>
-    </main>
-  );
+    return (
+        <main className="grid h-screen grid-cols-2">
+            <section className="bg-primary-600 rounded-br-3xl rounded-tr-3xl" />
+            <section className="flex items-center justify-center">{children}</section>
+        </main>
+    );
 };
