7ab0e53e76ce0c7cedc8d82c974cb1ece9818245
Ilham Rusydi Abd <abd.ilhamrusydi@gmail.com>
2024-10-06 01:28:26 +0700

    add google icon

--- src/features/authentication/components/login.tsx
+++ src/features/authentication/components/login.tsx
@@ -1,47 +1,61 @@
-import { Input } from "../../../components/ui/input";
-import { Button } from "../../../components/ui/button";
-import { AuthLayout } from "./auth.layout";
-import { AtSign, Lock } from "lucide-react";
-import { useState } from "react";
-import { useMutation } from "@tanstack/react-query";
-import { loginUser } from "../services/login";
-import { useNavigate } from "react-router-dom";
+import { Input } from '../../../components/ui/input';
+import { Button } from '../../../components/ui/button';
+import { AuthLayout } from './auth.layout';
+import { AtSign, EyeOffIcon } from 'lucide-react';
+import { useState } from 'react';
+import { useMutation } from '@tanstack/react-query';
+import { loginUser } from '../services/login';
+import { useNavigate } from 'react-router-dom';
+import { GoogleIcon } from '../../../components/ui/googleIcon';
 
 export const Login = () => {
-  const navigate = useNavigate();
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+    const navigate = useNavigate();
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
 
-  const {
-    mutate: handleSubmitLogin,
-    isPending,
-    isError,
-    error,
-  } = useMutation({
-    mutationKey: ["login"],
-    mutationFn: () => loginUser({ email, password }),
-    onSuccess: () => navigate("/"),
-  });
+    const {
+        mutate: handleSubmitLogin,
+        isPending,
+        isError,
+        error,
+    } = useMutation({
+        mutationKey: ['login'],
+        mutationFn: () => loginUser({ email, password }),
+        onSuccess: () => navigate('/'),
+    });
 
-  return (
-    <AuthLayout>
-      <div className="w-[300px] space-y-4">
-        <section>
-          <h3>Login</h3>
-          <p>welcome back!</p>
-        </section>
-        <section className="space-y-3">
-          <Input withIcon icon={<AtSign size={16} />} placeholder="Email" onChange={(e) => setEmail(e.target.value)} />
-          <Input withIcon icon={<Lock size={16} />} placeholder="Password" type="password" onChange={(e) => setPassword(e.target.value)} />
-          <Button disabled={isPending} onClick={() => handleSubmitLogin()} className="flex w-full justify-center">
-            Login
-          </Button>
-          <Button isFull variant="outline">
-            Continue with Google
-          </Button>
-        </section>
-        {isError && <div className="text-center text-sm font-medium text-red-500">{error.message}</div>}
-      </div>
-    </AuthLayout>
-  );
+    return (
+        <AuthLayout>
+            <div className="w-[300px] space-y-9">
+                <section className="space-y-2">
+                    <h3 className="text-center">Login</h3>
+                    <p className="text-center">welcome back!</p>
+                </section>
+                <section className="space-y-3">
+                    <Input withIcon icon={<AtSign size={16} />} placeholder="Email" onChange={(e) => setEmail(e.target.value)} />
+                    <Input withIcon icon={<EyeOffIcon size={16} />} placeholder="Password" type="password" onChange={(e) => setPassword(e.target.value)} />
+                    <Button disabled={isPending} onClick={() => handleSubmitLogin()} className="flex w-full justify-center">
+                        Login
+                    </Button>
+
+                    {/* separator */}
+                    <div className="flex flex-row gap-3 py-4">
+                        <div className="flex flex-col w-full justify-center">
+                            <div className="border rounded-xl border-gray-300 w-full"></div>
+                        </div>
+                        <p className="text-sm text-gray-500 text-nowrap">Or Login With</p>
+                        <div className="flex flex-col w-full justify-center">
+                            <div className="border rounded-xl border-gray-300 w-full"></div>
+                        </div>
+                    </div>
+
+                    {/* button with google */}
+                    <Button isFull variant="outline" startContent={<GoogleIcon />}>
+                        Continue with Google
+                    </Button>
+                </section>
+                {isError && <div className="text-center text-sm font-medium text-red-500">{error.message}</div>}
+            </div>
+        </AuthLayout>
+    );
 };
